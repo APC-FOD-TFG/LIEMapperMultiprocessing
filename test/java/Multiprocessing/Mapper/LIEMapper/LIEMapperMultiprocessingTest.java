7e8527f1cfd17051411c5b18dbc898b1a1bbd8c1
Ellioth <elliot22rx7@gmail.com>
2020-12-10 04:27:45 -0600

    added the unit testing #1
    we added the unit testing for a multiprocessing scenario and another for a serial scenario #1

--- test/java/Multiprocessing/Mapper/LIEMapper/LIEMapperMultiprocessingTest.java
+++ test/java/Multiprocessing/Mapper/LIEMapper/LIEMapperMultiprocessingTest.java
@@ -12,6 +12,8 @@ import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import testrun.config.ConfigurationTestRun;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 
 @DisplayName("LIE++ Mapper Unit testing module")
@@ -52,12 +54,58 @@ public class LIEMapperMultiprocessingTest {
      */
     @Test
     @DisplayName("-------------Testing the multiprocessing for LIE++-------------")
-    public void ExtensionTest(){
+    public void MultiprocessingTest(){
         System.out.println("-------------Testing the multiprocessing for LIE++-------------");
         try{
             ArrayList<String> finalSourceCode = Globals.fillTestArray(locations);
             LIEMapperMultiprocessing mmp = new LIEMapperMultiprocessing();
+
+            //start to measuring Time
+            Instant start = Instant.now();
+
             mmp.start(finalSourceCode);
+
+            //finish the measuring
+            Instant finish = Instant.now();
+            long timeElapsed = Duration.between(start, finish).toMillis();
+            System.out.println(" - - - Total elapsed time: " + timeElapsed+ " milli seconds");
+
+            //cleaning the global variables
+            Globals.reset();
+
+            junit.framework.Assert.assertTrue(true);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Error while trying to test the multiprocessing");
+        }
+    }
+
+    /**
+     * Method to test a serial implementation for the LIE++
+     * language.
+     */
+    @Test
+    @DisplayName("-------------Testing a serial implementation for LIE++-------------")
+    public void SerialTest(){
+        System.out.println("-------------Testing a serial implementation for LIE++-------------");
+        try{
+            ArrayList<String> finalSourceCode = Globals.fillTestArray(locations);
+
+            //start to measuring Time
+            Instant start = Instant.now();
+
+            for(String s : finalSourceCode){
+                Globals.mapSourceCode(s);
+            }
+
+            //finish the measuring
+            Instant finish = Instant.now();
+            long timeElapsed = Duration.between(start, finish).toMillis();
+            System.out.println(" - - - Total elapsed time: " + timeElapsed+ " milli seconds");
+
+            //cleaning the global variables
+            Globals.reset();
+
             junit.framework.Assert.assertTrue(true);
         }catch (Exception e){
             System.out.println(e.getMessage());
